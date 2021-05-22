1a0fbc6d8ce421b81b155dfee67a488133f4808a
bobbyZ94 <r.zioltkowski@gmail.com>
2021-05-22 16:07:20 +0200

    added chevronUp/Down to make Checkbox smaller

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-shadow */
@@ -26,6 +28,40 @@ export async function getStaticProps() {
 }
 
 export default function Index({ uebungen }) {
+  const chevronDown = (
+    <svg
+      className="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M19 9l-7 7-7-7"
+      />
+    </svg>
+  );
+  const chevronUp = (
+    <svg
+      className="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M5 15l7-7 7 7"
+      />
+    </svg>
+  );
+  const [visibleUebungstypen, setVisibleUebungstypen] = useState(true);
+  const [visibleMuskelgruppen, setVisibleMuskelgruppen] = useState(false);
   const [checkedAllUebungstypen, setCheckedAllUebungstypen] = useState(false);
   const [uebungstypChecked, setUebungstypChecked] = useState({
     Freihantel: true,
@@ -122,71 +158,97 @@ export default function Index({ uebungen }) {
         <div className="place-self-center xl:place-self-start xl:justify-self-center xl:sticky xl:top-20 mb-5 xl:mb-0">
           <div className="shadow-2xl rounded-2xl xl:mr-5 text-white bg-red-600 flex flex-col gap-2 xl:gap-4 p-4">
             <div>
-              <h3 className="inline mr-2">Übungstyp:</h3>
-              <div className="grid grid-cols-2 xl:grid-cols-1 gap-x-2">
-                {uebungstypen.map((uebungstyp) => (
-                  <div>
-                    <label>
-                      <input
-                        className="form-checkbox text-white h-4 w-4 align-middle"
-                        name={uebungstyp}
-                        type="checkbox"
-                        checked={uebungstypChecked[uebungstyp]}
-                        onChange={() => toggleCheckUebungstyp(uebungstyp)}
-                      />
-                      <span className="ml-1 align-middle">{uebungstyp}</span>
-                    </label>
-                  </div>
-                ))}
-                <div>
-                  <label>
-                    <input
-                      className="form-checkbox text-white h-4 w-4 align-middle"
-                      name="Mobility"
-                      type="checkbox"
-                      checked={checkedAllUebungstypen}
-                      onChange={(e) => selectAllUebungstypen(e.target.checked)}
-                    />
-                    <span className="ml-1 align-middle">Alle Übungstypen</span>
-                  </label>
+              <div
+                className="cursor-pointer flex justify-between"
+                onClick={() => setVisibleUebungstypen(!visibleUebungstypen)}
+              >
+                <h3 className="mr-2">Übungstypen</h3>
+                <div className="ml-1">
+                  {visibleUebungstypen ? chevronUp : chevronDown}
                 </div>
               </div>
-            </div>
-
-            <div>
-              <h3 className="mr-2">Muskelgruppe:</h3>
-              <div className="grid grid-cols-2 ss:grid-cols-3 xl:grid-cols-1 gap-x-2">
-                {muskelgruppen.map((muskelgruppe) => (
+              {visibleUebungstypen && (
+                <div className="grid grid-cols-2 xl:grid-cols-1 gap-x-2">
+                  {uebungstypen.map((uebungstyp) => (
+                    <div>
+                      <label>
+                        <input
+                          className="form-checkbox text-white h-4 w-4 align-middle"
+                          name={uebungstyp}
+                          type="checkbox"
+                          checked={uebungstypChecked[uebungstyp]}
+                          onChange={() => toggleCheckUebungstyp(uebungstyp)}
+                        />
+                        <span className="ml-1 align-middle">{uebungstyp}</span>
+                      </label>
+                    </div>
+                  ))}
                   <div>
                     <label>
                       <input
                         className="form-checkbox text-white h-4 w-4 align-middle"
-                        name={muskelgruppe}
+                        name="Mobility"
                         type="checkbox"
-                        checked={muskelgruppeChecked[muskelgruppe]}
-                        onChange={() => toggleCheckMuskelgruppe(muskelgruppe)}
+                        checked={checkedAllUebungstypen}
+                        onChange={(e) =>
+                          selectAllUebungstypen(e.target.checked)
+                        }
                       />
                       <span className="ml-1 align-middle">
-                        {muskelgruppe.replace('_', ' ')}
+                        Alle Übungstypen
                       </span>
                     </label>
                   </div>
-                ))}
-                <div className="col-span-2 ss:col-span-3 xl:col-span-1">
-                  <label>
-                    <input
-                      className="form-checkbox text-white h-4 w-4 align-middle"
-                      name="alle"
-                      type="checkbox"
-                      checked={checkedAllMuskelgruppen}
-                      onChange={(e) => selectAllMuskelgruppen(e.target.checked)}
-                    />
-                    <span className="ml-1 align-middle">
-                      Alle Muskelgruppen
-                    </span>
-                  </label>
+                </div>
+              )}
+            </div>
+
+            <div>
+              <div
+                className="cursor-pointer flex justify-between"
+                onClick={() => setVisibleMuskelgruppen(!visibleMuskelgruppen)}
+              >
+                <h3 className="mr-2">Muskelgruppen</h3>
+                <div className="ml-1">
+                  {visibleMuskelgruppen ? chevronUp : chevronDown}
                 </div>
               </div>
+              {visibleMuskelgruppen && (
+                <div className="grid grid-cols-2 xl:grid-cols-1 gap-x-2">
+                  {muskelgruppen.map((muskelgruppe) => (
+                    <div>
+                      <label>
+                        <input
+                          className="form-checkbox text-white h-4 w-4 align-middle"
+                          name={muskelgruppe}
+                          type="checkbox"
+                          checked={muskelgruppeChecked[muskelgruppe]}
+                          onChange={() => toggleCheckMuskelgruppe(muskelgruppe)}
+                        />
+                        <span className="ml-1 align-middle">
+                          {muskelgruppe.replace('_', ' ')}
+                        </span>
+                      </label>
+                    </div>
+                  ))}
+                  <div className="col-span-2 xl:col-span-1">
+                    <label>
+                      <input
+                        className="form-checkbox text-white h-4 w-4 align-middle"
+                        name="alle"
+                        type="checkbox"
+                        checked={checkedAllMuskelgruppen}
+                        onChange={(e) =>
+                          selectAllMuskelgruppen(e.target.checked)
+                        }
+                      />
+                      <span className="ml-1 align-middle">
+                        Alle Muskelgruppen
+                      </span>
+                    </label>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
