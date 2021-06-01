4a65cdd1b8a9fbf70444bd74cf468414b122c4c5
bobbyZ94 <r.zioltkowski@gmail.com>
2021-06-01 16:45:02 +0200

    added search functionality for uebungstyp and muskelgruppe

--- functions/filterAndUnionUebungsnameAndTags.js
+++ functions/filterAndUnionUebungsnameAndTags.js
@@ -1,8 +1,11 @@
+/* eslint-disable array-callback-return */
 export default function filterAndUnionUebungsnameAndTags(uebungen, keyword) {
   const resultUebungsname = uebungen.filter((uebung) =>
     uebung.fields.uebungsname.toLowerCase().includes(keyword.toLowerCase())
   );
-  // eslint-disable-next-line array-callback-return
+  const resultUebungstyp = uebungen.filter((uebung) =>
+    uebung.fields.uebungstyp.toLowerCase().includes(keyword.toLowerCase())
+  );
   const resultTags = uebungen.filter((uebung) => {
     for (let i = 0; i < uebung.fields.tags.length; i += 1) {
       if (uebung.fields.tags[i].toLowerCase().includes(keyword.toLowerCase())) {
@@ -10,6 +13,24 @@ export default function filterAndUnionUebungsnameAndTags(uebungen, keyword) {
       }
     }
   });
-  const union = [...new Set([...resultUebungsname, ...resultTags])];
+  const resultMuskelgruppen = uebungen.filter((uebung) => {
+    for (let i = 0; i < uebung.fields.muskelgruppe.length; i += 1) {
+      if (
+        uebung.fields.muskelgruppe[i]
+          .toLowerCase()
+          .includes(keyword.toLowerCase())
+      ) {
+        return true;
+      }
+    }
+  });
+  const union = [
+    ...new Set([
+      ...resultUebungsname,
+      ...resultTags,
+      ...resultUebungstyp,
+      ...resultMuskelgruppen,
+    ]),
+  ];
   return union;
 }
